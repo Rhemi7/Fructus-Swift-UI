9b9e4b88d8dfbc4013d1ab1e3d7c0ef6742df6d2
aderemilekun <adelefatai@yahoo.com>
2023-11-27 18:51:12 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aderemilekun Adele on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
