d37489c09a126edb969638929485064b9edc2600
aderemilekun <adelefatai@yahoo.com>
2023-12-02 13:52:22 +0100

    Fruits nutrient view showing the nutrient level of every fruit

---
+++ Fructus/View/FruitsNutrientsView.swift
@@ -0,0 +1,50 @@
+//
+//  FruitsNutrientsView.swift
+//  Fructus
+//
+//  Created by Aderemilekun Adele on 01/12/2023.
+//
+
+import SwiftUI
+
+struct FruitsNutrientsView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0 ..< nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[0])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 25)
+                        
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct FruitsNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitsNutrientsView(fruit: fruitsData[0])
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
+    }
+}
